edfa030ceee25a773bb202986f4794d89451c5a4
vhavlena <vohavlena@gmail.com>
2018-08-17 09:59:25 +0200

    Mona automata wrapper: symbol conversion

--- src/MonaAutomataWrapper.hs
+++ src/MonaAutomataWrapper.hs
@@ -9,17 +9,19 @@ module MonaAutomataWrapper where
 
 import MonaAutomataParser
 import TreeAutomaton
+import Data.List
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Alphabet as Alp
 
-monaGTAToTA :: MonaGTA -> BATreeAutomaton MonaState String
+monaGTAToTA :: MonaGTA -> BATreeAutomaton MonaState Alp.Symbol
 monaGTAToTA (MonaGTA header (MonaGuide guide) spaces) = BATreeAutomaton states roots leaves trans where
   guide' = Map.fromList $ map (\(MonaGuideRule _ fr dest) -> (fr, dest)) guide
-  expSpaces = expandTrans spaces
+  vars = variables header
+  expSpaces = expandTrans vars spaces
   sizes = getSizes expSpaces
   sizedict = getSizesDict expSpaces sizes
-  trans = unifyTransitions guide' sizedict expSpaces
+  trans = unifyTransitions vars guide' sizedict expSpaces
   states = Set.fromList [0..(last sizes)-1]
   leaves = Set.fromList $ zipWith (+) sizes $ map (initial) expSpaces
   roots = Set.fromList $ accept header
@@ -34,32 +36,42 @@ getSizes :: [MonaStateSpace] -> [Int]
 getSizes spaces = init . scanl (+) 0 $ map (size) spaces
 
 
-unifyTransitions ::  Map.Map MonaState [MonaState] -> Map.Map Int Int -> [MonaStateSpace] -> Map.Map ([MonaState],MonaSymbol) (Set.Set MonaState)
-unifyTransitions guide sizes spaces = Map.fromListWith (Set.union) $ foldr1 (++) $ map (unifyStateSpace guide sizes) spaces
+unifyTransitions :: [Alp.Variable] -> Map.Map MonaState [MonaState] -> Map.Map Int Int -> [MonaStateSpace] -> Map.Map ([MonaState],Alp.Symbol) (Set.Set MonaState)
+unifyTransitions vars guide sizes spaces = Map.fromListWith (Set.union) $ foldr1 (++) $ map (unifyStateSpace vars guide sizes) spaces
 
 
-unifyStateSpace :: Map.Map MonaState [MonaState] -> Map.Map Int Int -> MonaStateSpace  -> [(([MonaState],MonaSymbol), Set.Set MonaState)]
-unifyStateSpace guide sizes (MonaStateSpace iden _ _ initial trans) = map (conv) trans where
-  conv (MonaTransition src sym dest) = ((src',sym), Set.singleton dest) where
+unifyStateSpace :: [Alp.Variable] -> Map.Map MonaState [MonaState] -> Map.Map Int Int -> MonaStateSpace  -> [(([MonaState],Alp.Symbol), Set.Set MonaState)]
+unifyStateSpace vars guide sizes (MonaStateSpace iden _ _ initial trans) = map (conv) trans where
+  conv (MonaTransition src sym dest) = ((src', convToSymbol vars sym), Set.singleton dest) where
     pl = Map.findWithDefault [] dest guide
     sizes' = map (\a -> Map.findWithDefault 0 a sizes) pl
     src' = zipWith (+) src sizes'
 
 
-expandTrans :: [MonaStateSpace] -> [Alp.Variable] -> [MonaStateSpace]
-expandTrans = map (conv) where
-  conv (MonaStateSpace i n s ini tr) = MonaStateSpace i n s ini (tr >>= expandSymbols)
+expandTrans :: [Alp.Variable] -> [MonaStateSpace] -> [MonaStateSpace]
+expandTrans vars = map (conv) where
+  conv (MonaStateSpace i n s ini tr) = MonaStateSpace i n s ini (tr >>= expandSymbols vars)
 
 
-expandSymbols :: MonaTransition -> [MonaTransition]
-expandSymbols (MonaTransition src sym dest) = map (\s -> MonaTransition src s dest) expX where
-  expX = expandX sym
+expandSymbols :: [Alp.Variable] -> MonaTransition -> [MonaTransition]
+expandSymbols vars (MonaTransition src sym dest) = map (\s -> MonaTransition src s dest) expX where
+  expX = expandX $ reorderMonaSymbol vars sym
 
 
 expandX :: String -> [String]
 expandX str = listProd (reverse $ str >>= gen) [[]] where
   gen a = if a == 'X' then return ['0','1']
          else return [a]
+
+
+convToSymbol :: [Alp.Variable] -> String -> Alp.Symbol
+convToSymbol vars sym = (sym, Set.fromList vars)
+
+
+reorderMonaSymbol :: [Alp.Variable] -> String -> String
+reorderMonaSymbol vars sym = sym' where
+  pair = zip vars sym
+  sym' = map (snd) $ sortBy (\x y -> compare (fst x) (fst y)) pair
 
 
 listProd [] a = a
